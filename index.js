6a18ad3f29f3f5e986a10ba024ec1f32a2de6dde
coopwoods1 <cooperawoodson@icloud.com>
2026-09-15 23:11:54 +0000

    Configure server for Render

--- index.js
+++ index.js
@@ -164,4 +164,4 @@ server.on("listening", () => {
   console.log(chalk.green(`🌍 Server is running on http://localhost:${PORT}`));
 });
 
-server.listen({ port: PORT });
+server.listen({ port: PORT, host: "0.0.0.0" });
